f04a047bfe8fafbc22d7d30797655d4c84807423
Ivan Dimitrov <40958010+inweblife@users.noreply.github.com>
2021-07-02 18:39:26 +0300

    Update modern-site.js

--- pages/modern-site.js
+++ pages/modern-site.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/Link';
+import Link from 'next/link';
 import styles from '../styles/modern-site.module.css';
 
 const modern = () => {
