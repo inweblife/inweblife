8e74a45fa91014de4251eb97990e57013c8ea82b
Ivan Dimitrov <40958010+inweblife@users.noreply.github.com>
2023-03-22 10:39:33 +0200

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import styles from "../styles/blog.module.css";
+import styles from "../styles/Blog.module.css";
 
 const Blog = () => {
   return (
