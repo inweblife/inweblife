841b7d6a145d5340fdd6648c1c1ee984a8885ed0
Ivan Dimitrov <40958010+inweblife@users.noreply.github.com>
2024-11-29 23:02:02 +0200

    add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
       <SpeedInsights />
     </Layout>
   )
