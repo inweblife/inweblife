7e30d33ba24a370321790d822e8dc00f237868a0
Ivan Dimitrov <40958010+inweblife@users.noreply.github.com>
2025-04-06 12:53:41 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -8,8 +8,14 @@ const About = () => {
   return (
     <>
       <Head>
-        <title> За мен | Как се прави сайт за най-високо класиране в Google</title>
-        <meta name="description" content="Успешно SEO гарантира благополучно представяне на сайта в мрежата, за да се вижда от всички навсякъде." />
+        <title>
+          {" "}
+          За мен | Как се прави сайт за най-високо класиране в Google
+        </title>
+        <meta
+          name="description"
+          content="Успешно SEO гарантира благополучно представяне на сайта в мрежата, за да се вижда от всички навсякъде."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.webp" />
       </Head>
@@ -35,10 +41,13 @@ const About = () => {
           <div className={styles.paragraph}>
             <p>
               Казвам се Иван Димитров и съм роден през 1977 година в град София,
-              България. Занимавам се <Link href="/"><strong>Web development</strong></Link> от 2011 година. Завършил съм
-              престижната школа w3schools през 2018 година, Frontend masters,
-              както и някои руски школи за програмиране, Responsive design и Web
-              development.
+              България. Занимавам се{" "}
+              <Link href="/">
+                <strong>Web development</strong>
+              </Link>{" "}
+              от 2011 година. Завършил съм престижната школа w3schools през 2018
+              година, Frontend masters, както и някои руски школи за
+              програмиране, Responsive design и Web development.
             </p>
 
             <p>
@@ -51,11 +60,25 @@ const About = () => {
             </p>
 
             <p>
-              Имам опит с <Link href="/how-to-make-site"><strong>изграждането на Web сайтове</strong></Link> в най-различни сфери от
-              бизнеса и тяхното функционално настройване за успешни маркетингови
-              активности, SEO и PPC кампании, съобразени със създаването на
-              Facebook Business page и цялата палитра маркетингови услуги на
-              Google. Един от сайтове, по които работя е <Link href="https://kolazascrap.com/"><strong>Коли за скрап</strong></Link>, а друг - <Link href="https://ecometal.bg/"><strong>Коли за бракуване</strong></Link> и <Link href="https://carco.bg/"><strong>Авточасти втора употреба</strong></Link>
+              Имам опит с{" "}
+              <Link href="/how-to-make-site">
+                <strong>изграждането на Web сайтове</strong>
+              </Link>{" "}
+              в най-различни сфери от бизнеса и тяхното функционално настройване
+              за успешни маркетингови активности, SEO и PPC кампании, съобразени
+              със създаването на Facebook Business page и цялата палитра
+              маркетингови услуги на Google. Един от сайтове, по които работя е{" "}
+              <Link href="https://kolazascrap.com/">
+                <strong>Коли за скрап</strong>
+              </Link>
+              , а друг -{" "}
+              <Link href="https://ecometal.bg/">
+                <strong>Коли за бракуване</strong>
+              </Link>{" "}, освен това <Link href="https://kolizascrap.com/"><strong>Калкулатор коли за скрап </strong></Link>
+              и{" "}
+              <Link href="https://carco.bg/">
+                <strong>Авточасти втора употреба</strong>
+              </Link>
             </p>
           </div>
         </section>
@@ -68,10 +91,17 @@ const About = () => {
         <section className={styles.text}>
           <div className={styles.paragraph}>
             <p>
-              Убеден съм, че <Link href="/powerful-site"><strong>сайтовете трябва да се правят</strong></Link> от първия до
-              последния код, изцяло според най-добрите практики на Google за
-              оптимизация, от заглавията, през описанията по ключови думи, чак
-              до скорост на зареждане и най-приятно потребителско изживяване. Повече неща ще разказвам в <Link href="/blog"><strong>моя блог</strong></Link> 
+              Убеден съм, че{" "}
+              <Link href="/powerful-site">
+                <strong>сайтовете трябва да се правят</strong>
+              </Link>{" "}
+              от първия до последния код, изцяло според най-добрите практики на
+              Google за оптимизация, от заглавията, през описанията по ключови
+              думи, чак до скорост на зареждане и най-приятно потребителско
+              изживяване. Повече неща ще разказвам в{" "}
+              <Link href="/blog">
+                <strong>моя блог</strong>
+              </Link>
             </p>
 
             <div className={styles.picture}>
