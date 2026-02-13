703eb6e492598ea3c731e0f68e17063c810a4112
inweblife <inweblife@yandex.com>
2026-02-13 16:44:40 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="bg">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="sfRPcXRVIhRKjhZZpZq4w-Z2eWaHcXE8C-G-Ntxkrd0"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
