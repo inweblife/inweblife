e3b27f754d73e98938047e314e1913ab185195a9
inweblife <inweblife@yandex.com>
2026-04-12 13:38:47 +0300

    add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout'
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from "next/font/google"
