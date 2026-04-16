3a6c166bc1ddd63184f7b6d619b0fcc7617f43f9
inweblife <inweblife@yandex.com>
2026-04-16 15:17:51 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -58,6 +58,23 @@ const Footer = () => {
             <li>
               <a
                 className="socialLink"
+                href="https://www.youtube.com/@inweblife"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="YouTube"
+              >
+                <svg className="socialIcon" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                  <path
+                    fill="currentColor"
+                    d="M23.5 6.19a3.02 3.02 0 00-2.12-2.14C19.54 3.5 12 3.5 12 3.5s-7.54 0-9.38.55A3.02 3.02 0 00.5 6.19C0 8.04 0 12 0 12s0 3.96.5 5.81a3.02 3.02 0 002.12 2.14C4.46 20.5 12 20.5 12 20.5s7.54 0 9.38-.55a3.02 3.02 0 002.12-2.14C24 15.96 24 12 24 12s0-3.96-.5-5.81zM9.75 15.5v-7l6.5 3.5-6.5 3.5z"
+                  />
+                </svg>
+                <span className="srOnly">YouTube</span>
+              </a>
+            </li>
+            <li>
+              <a
+                className="socialLink"
                 href="/rss.xml"
                 rel="alternate"
                 type="application/rss+xml"
