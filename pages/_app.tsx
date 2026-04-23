6cf5ff4a493123c0ef5cf27657f876be89723628
inweblife <inweblife@yandex.com>
2026-04-23 12:45:53 +0300

    styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,24 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter } from "next/font/google"
+import { Inter, Space_Grotesk } from "next/font/google"
 
 const inter = Inter({
   subsets: ["latin", "cyrillic"],
   display: "swap",
+  variable: "--font-body",
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["700", "800"],
+  display: "swap",
+  variable: "--font-brand",
 })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
+    <div className={`${inter.variable} ${spaceGrotesk.variable} ${inter.className}`}>
       <Layout>
         <Component {...pageProps} />
         <Analytics />
