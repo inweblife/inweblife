1094af9b12930e01066869dd3f86edee7f08aac8
inweblife <inweblife@yandex.com>
2026-04-23 12:48:42 +0300

    redesign

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter, Space_Grotesk } from "next/font/google"
+import { Inter, Space_Grotesk, Manrope } from "next/font/google"
 
 const inter = Inter({
   subsets: ["latin", "cyrillic"],
@@ -18,9 +18,16 @@ const spaceGrotesk = Space_Grotesk({
   variable: "--font-brand",
 })
 
+const manrope = Manrope({
+  subsets: ["latin", "cyrillic"],
+  weight: ["600", "700"],
+  display: "swap",
+  variable: "--font-heading",
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${inter.variable} ${spaceGrotesk.variable} ${inter.className}`}>
+    <div className={`${inter.variable} ${spaceGrotesk.variable} ${manrope.variable} ${inter.className}`}>
       <Layout>
         <Component {...pageProps} />
         <Analytics />
