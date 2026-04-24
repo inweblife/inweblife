acaf1d6ca95dbc5b0bb7fa20e537de4e383aeb8b
inweblife <inweblife@yandex.com>
2026-04-24 14:22:26 +0300

    add robots

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      { source: '/robots.txt', destination: '/api/robots' },
       { source: '/sitemap.xml', destination: '/api/sitemap' },
       { source: '/rss.xml', destination: '/api/rss' },
     ];
